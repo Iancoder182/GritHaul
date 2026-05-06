9e52a1125f4e4f4d7cba32232b81fd11a1ab79ec
Iancoder182 <mwendaian182@gmail.com>
2026-05-06 19:55:29 +0300

    Update DriverViewModel.kt

--- app/src/main/java/com/ian/grithaul/viewmodel/DriverViewModel.kt
+++ app/src/main/java/com/ian/grithaul/viewmodel/DriverViewModel.kt
@@ -1,2 +1,17 @@
-package com.ian.grithaul.viewmodel
+package com.ian.grithaul.ui.viewmodels
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.ian.grithaul.data.GritRepository
+import com.ian.grithaul.data.local.entities.PickupEntity
+import kotlinx.coroutines.launch
+
+class DriverViewModel(private val repository: GritRepository) : ViewModel() {
+
+    fun markPickupAsCompleted(pickup: PickupEntity) {
+        viewModelScope.launch {
+            val updated = pickup.copy(status = "Completed")
+            repository.updatePickupStatus(updated)
+        }
+    }
+}
